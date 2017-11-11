38be415d56146c78ab66c148c3e501da041d3af2
marinasiena <marinasiena123@gmail.com>
2017-11-11 11:44:48 -0500

    Uploading initial create two servers

---
+++ twoservers.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var PORT1 = 7000;
+var PORT2 = 7500;
+function handleRequest1(request,response){
+	response.end("it works! Path hit: " + request.url);
+}
+function handleRequest2(request,response){
+	response.end("it works! Path hit: " + request.url);
+}
+
+var server1 = http.createServer(handleRequest1);
+var server2 = http.createServer(handleRequest2);
+
+server1.listen(PORT1, function(){
+	console.log("working1")
+});
+
+server2.listen(PORT2, function(){
+	console.log("working2")
+});
